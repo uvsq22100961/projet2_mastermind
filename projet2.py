2ae6a3dfc75962a84e7b3d0626804e063aa78055
Camille ROESSLER <camille.roessler@ens.uvsq.fr>
2022-04-18 12:49:14 +0200

    modification quadrillage + quadrillage 2

--- projet2.py
+++ projet2.py
@@ -13,9 +13,9 @@ import random as rd
 
 # variables 
 
-x0 = 70 
+x0 = 110 
 y0 = 100
-x1 = 120 
+x1 = 160 
 y1 =  150
 
 code = []
@@ -45,9 +45,17 @@ def quadrillage() :
     global x1, y1 
     for i in range (10) :  
         for l in range (4) : 
-            canvas.create_rectangle((x0 + l*50, y0), (x1 + l*50, y1))
+            canvas.create_rectangle((x0 + l*50, y0), (x1 + l*50, y1), fill = "white")
+            canvas.create_oval((x0 + l*50, y0), (x1 + l*50, y1))
         y0 = y1 
-        y1 = y1 + 50       
+        y1 = y1 + 50    
+
+def quadrillage2() : 
+    """fonction qui crée les ronds où l'utilisateur cliquera pour choisir la couleur de ses pions"""   
+    for i in range (8) : 
+        a = couleurs_Gpion[i]
+        canvas.create_rectangle((10 + (i*50), 20), (60 + (i*50), 70), fill = "white")
+        canvas.create_oval((10 + (i*50), 20), (60 + (i*50), 70), fill = a)
 
 
 # création des widgets 
@@ -56,6 +64,7 @@ canvas = tk.Canvas(racine, height= HAUTEUR, width= LARGEUR, bg="grey")
 
 liste = []
 quadrillage()
+quadrillage2()
 
 # placement des widgets 
 canvas.grid()
