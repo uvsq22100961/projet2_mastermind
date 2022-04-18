53c79c5458b77099fb5bf51965bcf05a63e9c55e
Camille ROESSLER <camille.roessler@ens.uvsq.fr>
2022-04-18 08:29:57 +0200

    style du code

--- projet2.py
+++ projet2.py
@@ -1,29 +1,38 @@
-# Projet Mastermind
+####################################
+# Projet Mastermind MITD2 groupe 4 
 # Thibault ROYERE
 # Camille ROESLER
 # Manira MAHAMAT HAGGAR
+####################################
 
-############################
 
-# BIBLIOTHEQUE
+# import des bibliothèques 
+
 import tkinter as tk
 import random as rd
 
-# VARIABLES
-couleurs_Gpion = ["green", "red", "yellow", "blue", "white", "pink", "orange", "purple"]
-couleur_Ppion = ["yellow", "red"]
+# variables 
+
 code = []
 LARGEUR = 500
 HAUTEUR = 800
 
-# FONCTION
+# variables globales 
+
+couleurs_Gpion = ["green", "red", "yellow", "blue", "white", "pink", "orange", "purple"]
+couleur_Ppion = ["yellow", "red"]
+
+###########
+# fonctions
 
 def choix_code1():
+    """fonction qui permet à l'ordinateur de choisir le code en mode 1 joueur """
     global couleur_Gpion
     global code
     for i in range(4):
         c = rd.choice(couleurs_Gpion)
         code.append(c)
+
 
 def quadrillage():
     """fonction qui crée le quadrillage"""
@@ -33,12 +42,15 @@ def quadrillage():
             canvas.create_line((50 + ligne * HAUTEUR//12,70),(50 + ligne * HAUTEUR//12, HAUTEUR - 50))
 
 
-#
+# création des widgets 
 racine = tk.Tk()
 canvas = tk.Canvas(racine, height= HAUTEUR, width= LARGEUR)
-canvas.grid()
+
 liste = []
 quadrillage()
 
-#
+# placement des widgets 
+canvas.grid()
+
+# boucle principale 
 canvas.mainloop()
